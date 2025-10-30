c6a820fc00119830c01c045e00a879759798f944
Ibuki.O <isan.ethen@gmail.com>
2025-10-30 15:38:16 +0900

    feat: Add switching ffi or not.

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ use quote::quote;
 use std::fs::OpenOptions;
 use std::io::Write;
 use syn::{
-    parse::{Parse, ParseStream, Result},
-    parse_macro_input, ItemFn, LitStr,
+    Expr, Ident, ItemFn, Lit, Meta, Token,
+    parse::{Parse, ParseStream, Parser, Result},
+    parse_macro_input,
+    punctuated::Punctuated,
 };
 
 fn log_unimplemented_function(func_name: &str, comment: &str) {
@@ -32,25 +34,83 @@ fn log_unimplemented_function(func_name: &str, comment: &str) {
 
 #[proc_macro_attribute]
 pub fn unimplemented_function(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let args_result = Punctuated::<Expr, Token![,]>::parse_terminated.parse(attr);
+    let args = match args_result {
+        Ok(args) => args,
+        Err(e) => return e.to_compile_error().into(),
+    };
+
     let func = parse_macro_input!(item as ItemFn);
 
-    let comment = if !attr.is_empty() {
-        let lit_str = parse_macro_input!(attr as LitStr);
-        lit_str.value()
-    } else {
-        String::new()
-    };
+    let mut comment = String::new();
+    let mut is_ffi = false;
+
+    for arg in args {
+        match arg {
+            Expr::Lit(expr_lit) => {
+                if let Lit::Str(lit_str) = &expr_lit.lit {
+                    comment = lit_str.value();
+                }
+            }
+            Expr::Assign(expr_assign) => {
+                let left = &expr_assign.left;
+                let right = &expr_assign.right;
+
+                if let Expr::Path(expr_path) = &**left {
+                    if expr_path.path.is_ident("ffi") {
+                        if let Expr::Lit(expr_lit) = &**right {
+                            if let Lit::Bool(lit_bool) = &expr_lit.lit {
+                                is_ffi = lit_bool.value;
+                            } else {
+                                return syn::Error::new_spanned(
+                                    right,
+                                    "Expected boolean literal (true or false) for ffi",
+                                )
+                                .to_compile_error()
+                                .into();
+                            }
+                        } else {
+                            return syn::Error::new_spanned(
+                                right,
+                                "Expected boolean literal (true or false) for ffi",
+                            )
+                            .to_compile_error()
+                            .into();
+                        }
+                    }
+                }
+            }
+            _ => {
+                return syn::Error::new_spanned(
+                    arg,
+                    "Unsupported attribute argument. Use \"comment\" or ffi = true",
+                )
+                .to_compile_error()
+                .into();
+            }
+        }
+    }
 
     let func_name = func.sig.ident.to_string();
     log_unimplemented_function(&func_name, &comment);
 
     let vis = &func.vis;
     let sig = &func.sig;
-    let result = quote! {
-        #[doc = #comment]
-        #[unsafe(no_mangle)]
-        #vis unsafe #sig {
-            unimplemented!()
+
+    let result = if is_ffi {
+        quote! {
+            #[doc = #comment]
+            #[unsafe(no_mangle)]
+            #vis unsafe extern "C" #sig {
+                unimplemented!()
+            }
+        }
+    } else {
+        quote! {
+            #[doc = #comment]
+            #vis #sig {
+                unimplemented!()
+            }
         }
     };
 
@@ -58,22 +118,50 @@ pub fn unimplemented_function(attr: TokenStream, item: TokenStream) -> TokenStre
 }
 
 struct UnimplementedInput {
+    is_ffi: bool,
     functions: Vec<ItemFn>,
 }
 
 impl Parse for UnimplementedInput {
     fn parse(input: ParseStream) -> Result<Self> {
+        let mut is_ffi = false;
+
+        if input.peek(Ident) && input.peek2(Token![=]) {
+            let ident: Ident = input.parse()?;
+            if ident == "ffi" {
+                input.parse::<Token![=]>()?;
+                if let Lit::Bool(lit_bool) = input.parse::<Lit>()? {
+                    is_ffi = lit_bool.value;
+                } else {
+                    return Err(syn::Error::new(
+                        ident.span(),
+                        "Expected boolean literal (true or false) after 'ffi ='",
+                    ));
+                }
+
+                if input.peek(Token![,]) {
+                    input.parse::<Token![,]>()?;
+                }
+            } else {
+                return Err(syn::Error::new(
+                    ident.span(),
+                    "Expected 'ffi = true' or function definitions",
+                ));
+            }
+        }
+
         let mut functions = Vec::new();
         while !input.is_empty() {
             functions.push(input.parse()?);
         }
-        Ok(UnimplementedInput { functions })
+
+        Ok(UnimplementedInput { is_ffi, functions })
     }
 }
 
 #[proc_macro]
 pub fn unimplemented_functions(input: TokenStream) -> TokenStream {
-    let UnimplementedInput { functions } = parse_macro_input!(input as UnimplementedInput);
+    let UnimplementedInput { is_ffi, functions } = parse_macro_input!(input as UnimplementedInput);
 
     let generated_functions = functions.iter().map(|func| {
         let func_name = func.sig.ident.to_string();
@@ -81,10 +169,19 @@ pub fn unimplemented_functions(input: TokenStream) -> TokenStream {
 
         let vis = &func.vis;
         let sig = &func.sig;
-        quote! {
-            #[unsafe(no_mangle)]
-            #vis unsafe #sig {
-                unimplemented!()
+
+        if is_ffi {
+            quote! {
+                #[unsafe(no_mangle)]
+                #vis unsafe extern "C" #sig {
+                    unimplemented!()
+                }
+            }
+        } else {
+            quote! {
+                #vis #sig {
+                    unimplemented!()
+                }
             }
         }
     });
