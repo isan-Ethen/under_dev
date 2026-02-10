29901f6cbe2558fb56ebbce0ccc867a8bbc16818
Ibuki.O <isan.ethen@gmail.com>
2026-02-10 12:57:33 +0900

    chore: Use env var COOKBOOK_LOG.

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use syn::{
 };
 
 fn log_unimplemented_function(func_name: &str, comment: &str, file_info: &str) {
-    let out_dir = "/home/isan/redox".to_string();
+    let out_dir = std::env::var("COOKBOOK_LOGS").unwrap();
     let dest_path = std::path::Path::new(&out_dir).join("under_devs.txt");
 
     let mut file = OpenOptions::new()
