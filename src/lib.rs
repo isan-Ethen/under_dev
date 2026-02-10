164caff80bcf558d828d9678cf8caadaa0080152
Ibuki.O <isan.ethen@gmail.com>
2026-02-10 13:00:38 +0900

    wip.

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use syn::{
 };
 
 fn log_unimplemented_function(func_name: &str, comment: &str, file_info: &str) {
-    let out_dir = std::env::var("COOKBOOK_LOGS").unwrap();
+    let out_dir = "/home/isan/redox".to_string();
     let dest_path = std::path::Path::new(&out_dir).join("under_devs.txt");
 
     let mut file = OpenOptions::new()
